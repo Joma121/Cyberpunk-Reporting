9d6d418089c1449879b3a12389317ea6ab88ae0d
Joma121 <martinsjoshua1986@gmail.com>
2020-12-18 15:39:48 -0800

    expands users routes to be functional

--- controllers/users.js
+++ controllers/users.js
@@ -11,7 +11,10 @@ const db = require("../models");
 // Index
 router.get("/", async (req, res) => {
     try {
-        return res.send("Main User Page loading");
+        const allUsers = await db.User.find();
+        const context = {users: allUsers};
+
+        return res.render("users/view-users", context);
     } catch (err) {
         return res.send(err);
     }
@@ -19,42 +22,60 @@ router.get("/", async (req, res) => {
 
 // New - will be covered by auth register
 
+// Edit
+router.get("/settings", authRequired, async (req, res) => {
+    try {
+        const foundUser = await db.User.findById(req.session.currentUser.id);
+        const context = { user: foundUser };
+
+        return res.render("users/accountSettings", context);
+    } catch (err) {
+        return res.send(err);        
+    }
+});
+
 // Show
 router.get("/:id", async (req, res) => {
     try {
-        return res.send("User Profile page");
+        const foundUser = await db.User.findById(req.params.id).populate("reports");
+        const context = {user: foundUser};
+
+        return res.render("users/usersProfile", context);
     } catch (err) {
         return res.send(err);
     }
 });
 
-// Create - will be covered by auth 
-
-// Edit
-router.get("/:id/edit", authRequired, async (req, res) => {
-    try {
-        return res.send("Edit User Profile");
-    } catch (err) {
-        return res.send(err);        
-    }
-})
+// Create - covered by auth register
 
 // Update
 router.put("/:id", authRequired, async (req, res) => {
     try {
-        return res.send();
+        const updatedUser = await db.User.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: {
+                    ...req.body
+                }
+            },
+            {new: true},
+        )
+        console.log("=== Modified ===\n", updatedUser);
+        return res.redirect(`/users/${updatedUser._id}`);
     } catch (err) {
         return res.send(err);
     }
-})
+});
 
 // Delete
 router.delete("/:id", authRequired, async (req, res) => {
     try {
-        return res.send();
+        const deletedUser = await db.User.findByIdAndDelete(req.params.id);
+        await db.Report.remove({createdBy: deletedUser._id});
+        return res.redirect("/");
     } catch (err) {
         return res.send(err);
     }
-})
+});
 
 module.exports = router;
