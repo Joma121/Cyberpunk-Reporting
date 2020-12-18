d951a3a7b109d1e155cf389e8efa554230bfb38a
Joma121 <martinsjoshua1986@gmail.com>
2020-12-17 18:27:59 -0800

    adds initial show route test

--- controllers/users.js
+++ controllers/users.js
@@ -6,12 +6,25 @@ const router = express.Router();
 const db = require("../models");
 
 /* Routes */
+// Index
 router.get("/", async (req, res) => {
     try {
         return res.send("Main User Page loading");
     } catch (err) {
         return res.send(err);
     }
-})
+});
+
+// New - will be covered by auth register
+
+// Show
+router.get("/:id", async (req, res) => {
+    try {
+        return res.send("User Profile page");
+    } catch (err) {
+        return res.send(err);
+    }
+});
+
 
 module.exports = router;
