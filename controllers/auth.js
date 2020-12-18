a3122baf23b92bdab66f5443af559f99537355de
Joma121 <martinsjoshua1986@gmail.com>
2020-12-18 12:06:30 -0800

    builds out and tested register and login auth routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,9 +9,17 @@ router.get("/register", (req, res) => {
 });
 
 // Register - Post
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
     try {
-        return res.send("registered!");
+        const foundUser = await db.User.findOne({ email: req.body.email });
+        if (foundUser) return res.redirect("/login");
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password, salt)
+        req.body.password = hash;
+        const newUser = await db.User.create(req.body);
+
+
+        return res.redirect("/login");
     } catch (err) {
         return res.send(err);
     }
@@ -23,9 +31,20 @@ router.get("/login", (req, res) => {
 });
 
 // Verify - post
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
     try {
-        return res.send("Login requested");
+        const foundUser = await db.User.findOne({ email: req.body.email });
+        if(!foundUser) return res.render("auth/login", {message: "Account Not Found. Please register."});
+
+        const match = await bcrypt.compare(req.body.password, foundUser.password);
+        if(!match)  return res.render("auth/login", {message:"Password or Email invalid."});
+
+        req.session.currentUser = {
+            id: foundUser._id,
+            username: foundUser.username,
+        }
+
+        return res.redirect("/");
     } catch (err) {
         return res.send(err);
     }
