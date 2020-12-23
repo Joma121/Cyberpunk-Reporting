99b1c8cf9e841da5713cb50bd4cd3b4be787b8ec
Joma121 <martinsjoshua1986@gmail.com>
2020-12-22 23:41:28 -0800

    adds click events for swapping settings display in edit profile

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,63 +8,50 @@ $(document).ready(function(){
     });
 });
 */
-$(document).ready(function(){
-    $("#post").click(function(){
-        $(window).attr('location','views/users/new.ejs');
-    });
+// $(document).ready(function(){
+//     $("#post").click(function(){
+//         $(window).attr('location','views/users/new.ejs');
+//     });
+// });
+
+// $(document).ready(function(){
+//     $("#home").click(function(){
+//         $(window).attr('location','views/home.ejs');
+//     });
+// });
+
+// $(document).ready(function(){
+//     $("#profile").click(function(){
+//         $(window).attr('location','views/users/usersProfile.ejs');
+//     });
+// });
+
+// /* Account Setting tabs*/
+// const tab8tn=document.querySelectorAll(".tab");
+// const tab = document.querySelectorAll(".tabShow");
+
+// function tabs(panelIndex) {
+//     tab.forEach(function(node) {
+//         node.style.display= "none"
+//     });
+//     tab[panelIndex].style.display ="block";
+
+// }
+// tabs(0);
+
+// $(".tab").onclick(function() {
+//     $(this).addClass("active").siblings().removeClass("active");
+// })
+
+
+
+/* Edit profile swap view*/
+$("#personal").click(function(){
+    $(".personal").show();
+    $(".account").hide();
 });
 
-$(document).ready(function(){
-    $("#home").click(function(){
-        $(window).attr('location','views/home.ejs');
-    });
-});
-
-$(document).ready(function(){
-    $("#profile").click(function(){
-        $(window).attr('location','views/users/usersProfile.ejs');
-    });
-});
-
-/* Account Setting tabs*/
-const tab8tn=document.querySelectorAll(".tab");
-const tab = document.querySelectorAll(".tabShow");
-
-function tabs(panelIndex) {
-    tab.forEach(function(node) {
-        node.style.display= "none"
-    });
-    tab[panelIndex].style.display ="block";
-
-}
-tabs(0);
-
-$(".tab").onclick(function() {
-    $(this).addClass("active").siblings().removeClass("active");
+$("#account").click(function(){
+    $(".account").show();
+    $(".personal").hide();
 })
-
-
-
-/*Profile onclick to account settings*/
-const tabtn=document.querySelectorAll(".side-nav ul li");
-const tab = document.querySelectorAll(".tabs");
-
-function show(panelIndex) {
-    tab.forEach(function(node) {
-        node.style.display= "none"
-    });
-    tab[panelIndex].style.display ="block";
-
-}
-show(0);
-
-$(".side-nav ul li").click(function() {
-    $(this).addClass("active").siblings().removeClass("active")
-})
-
-$(document).ready(function(){
-    $("button").click(function(){
-        $(window).attr('location','views/users/accountSettings.ejs');
-    });
-});
-
